4df004552108f1d8967f043640b86c27662ffb44
IbatullinaMaria <mpbugaga@yandex.ru>
2022-04-17 21:52:30 +0300

    add start

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-4">Это React-приложение</h1>
+            <p class="lead">
+                Версия приложения <strong>1.0.27</strong>
+            </p>
+        </div>
+    </div>
+)
